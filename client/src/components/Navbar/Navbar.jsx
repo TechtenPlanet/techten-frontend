2fb40f0ac7ad8cf3498f314389c69b1f441913a2
Cyber Guy <67350960+aidoomusahrazal@users.noreply.github.com>
2024-03-04 16:22:22 +0000

    Done with the screen width of 700px

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import style from './Navbar.module.css'
 import PrimaryButton from '../PrimaryButton/PrimaryButton'
 import SecondaryButton from '../SecondaryButton/SecondaryButton'
 import {Link} from 'react-router-dom'
 import logo from './../../assets/images/logo.png'
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 const Navbar = () => {
+  const [res, setRes] = useState(false)
+
+  const handleRes = () => {
+    setRes(!res)
+  }
   return (
     <div className={style.navbar}>
         <div className={style.navbarWrapper}>
@@ -33,11 +39,37 @@ const Navbar = () => {
                 <Link>Gallery</Link>
               </li>
             </ul>
+           
            </div>
            <div className={style.navbarBtn}>
             <SecondaryButton txt="Donate" />
             <PrimaryButton txt="Contact Us" />
            </div>
+           <div onClick={handleRes} className={style.resIcon}>
+              {!res ? <FaBars /> : <FaTimes />}
+            </div>
+        </div>
+        <div className={ res ? style.show : style.noShow }>
+        <ul>
+              <li>
+                <Link>Home</Link>
+              </li>
+              <li>
+                <Link>About Us</Link>
+              </li>
+              <li>
+                <Link>Services</Link>
+              </li>
+              <li>
+                <Link>Events</Link>
+              </li>
+              <li>
+                <Link>Blogs</Link>
+              </li>
+              <li>
+                <Link>Gallery</Link>
+              </li>
+            </ul>
         </div>
     </div>
   )
