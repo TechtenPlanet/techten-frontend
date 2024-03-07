eea6c1ac5124da498295d55ca3607715cef437f8
Cyber Guy <67350960+aidoomusahrazal@users.noreply.github.com>
2024-03-07 13:31:32 +0000

    almost done with the banner1

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -4,12 +4,14 @@ import Mission from '../components/Mission/Mission'
 import AboutSection from '../components/AboutSection/AboutSection'
 import ServicesSection from '../components/ServicesSection/ServicesSection'
 import EventsSection from '../components/EventsSection/EventsSection'
+import Banner1 from '../components/Banner1/Banner1'
 
 const HomePage = () => {
   return (
     <>
     <HomeHero />
     <Mission />
+    <Banner1 />
     <AboutSection />
     <ServicesSection />
     <EventsSection />
